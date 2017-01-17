3a934ec0b1938a957d7396aa20feb6923dd1f524
Ledoux <erwan.ledoux@snips.net>
2017-01-17 18:14:48 +0100

    added gzip for bundle in prod

--- bundler/prod.config.js
+++ bundler/prod.config.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const gzipSize = require('gzip-size')
+const path = require('path')
 const webpack = require('webpack')
 
 const config = require('./config')
@@ -13,11 +16,19 @@ module.exports = Object.assign(
       }
     ),
     plugins: [
-      new webpack.DefinePlugin(
-        {
-          'process.env': { NODE_ENV: `'${PRODUCTION}'` }
-        }
-      )
+      new webpack.DefinePlugin({'process.env': { NODE_ENV: `'${PRODUCTION}'` }}),
+      function () {
+        this.plugin('done', function (stats) {
+          const filename = stats.compilation.outputOptions.filename.replace('[hash]', stats.hash)
+          const filepath = path.join(stats.compilation.outputOptions.path, filename)
+          fs.readFile(filepath, (err, data) => {
+            if (err) { console.log('error reading js bundle', err) }
+            const byteSize = gzipSize.sync(data)
+            const kbSize = Math.round(byteSize / 1024)
+            console.log('\n\nGZIP size\n', filename + ': ~', kbSize, 'kB\n')
+          })
+        })
+      }
     ]
   }
 )
