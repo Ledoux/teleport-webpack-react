de7438b694c20f9683bcac356bc39f02dbfc617d
Jonas Ohlsson Aden <jonas.sebastian.ohlsson@gmail.com>
2017-01-26 18:40:49 +0100

    less noisy output from webpack

--- bundler/_p_hotAssetsServer.js
+++ bundler/_p_hotAssetsServer.js
@@ -12,7 +12,20 @@ new WebpackDevServer(
     contentBase: 'backend/servers/$[frontend.serverName]/app/templates/',
     hot: true,
     historyApiFallback: true,
-    publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+
+    // provide less noisy output from webpack
+    quiet: false,
+    noInfo: false,
+    stats: {
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
+    }
   }
 ).listen(PORT, HOST, function (err, result) {
   if (err) {
