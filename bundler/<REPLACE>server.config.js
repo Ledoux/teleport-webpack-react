30d64a913c062b6267dc7bfcbd99615e597299d4
erwanledoux <lawanledoux@gmail.com>
2017-06-08 19:16:21 +0200

    upgrade to webpack2

--- bundler/<REPLACE>server.config.js
+++ bundler/<REPLACE>server.config.js
@@ -2,6 +2,7 @@ const HOST = '0.0.0.0' // so we can test the project remotely over the same netw
 const PORT = 3000
 
 module.exports = {
+  contentBase: 'backend/servers/$[frontend.serverName]/app/templates/',
   host: HOST,
   port: PORT,
   url: `http://${HOST}:${PORT}`
