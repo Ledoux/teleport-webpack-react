2b76165ce476b7f8969990ef873837aa752957dc
erwanledoux <lawanledoux@gmail.com>
2017-07-22 17:59:10 +0200

    exclude node_modules for dev css

--- bundler/dev.config.js
+++ bundler/dev.config.js
@@ -18,7 +18,7 @@ module.exports = Object.assign({},
     module: {
       rules: config.module.rules.concat([{
           test: /\.s?css$/,
-          // exclude: /node_modules/,
+          exclude: /node_modules/,
           use: [
             'style-loader',
             'css-loader',
