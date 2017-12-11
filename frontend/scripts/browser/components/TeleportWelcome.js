d7fcfd39794532709460f1d52effa381fdff02eb
erwanledoux <lawanledoux@gmail.com>
2017-12-12 00:36:13 +0100

    made TeleportWelcome more responsive

--- frontend/scripts/browser/components/TeleportWelcome.js
+++ frontend/scripts/browser/components/TeleportWelcome.js
@@ -2,36 +2,36 @@ import React from 'react'
 
 const TeleportWelcome = ({templates}) => {
   return (
-    <div className="teleport center p2">
-      <div className="teleport__logo mb3">
-        <img className="teleport__logo__img" src="https://raw.githubusercontent.com/snipsco/teleport/master/icons/icon-white.png" />
+    <div className='teleport center p2'>
+      <div className='teleport__logo mb3'>
+        <img className='teleport__logo__img' src='https://raw.githubusercontent.com/snipsco/teleport/master/icons/icon-white.png' />
       </div>
-      <h2 className="mb1">
+      <h2 className='mb1'>
         Welcome!
       </h2>
-      <p className="teleport__text mb3">
+      <div className='teleport__text mb3'>
         This app was made using
-        <a className="teleport__text__href"
-          href="https://github.com/snipsco/teleport"
-          target="_blank" >
+        <a className='teleport__text__href'
+          href='https://github.com/snipsco/teleport'
+          target='_blank' >
           Teleport
         </a>
-      </p>
-      <div className="teleport__templates p2 mb3">
-        <div className="flex justify-center items-center">
+      </div>
+      <div className='teleport__templates p2 mb3'>
+        <div className='flex justify-center items-center'>
           {
             templates.map((template, index) => (
-              <div key={index} className="mr2">
+              <div key={index} className='mr2'>
                <a title={template.name}
                  href={template.gitUrl} >
-                  <img src={template.iconUrl} />
+                  <img className='teleport__templates__img' src={template.iconUrl} />
                </a>
               </div>
             ))
           }
         </div>
       </div>
-      <div className="teleport__footer">
+      <div className='teleport__footer'>
         From the Snips Tooling Team
       </div>
     </div>
