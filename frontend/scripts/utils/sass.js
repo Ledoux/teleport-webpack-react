426bc7842811d496e787a2df433c6ef5f9981e72
erwanledoux <lawanledoux@gmail.com>
2017-12-12 00:35:44 +0100

    sass with better colors

--- frontend/scripts/utils/sass.js
+++ frontend/scripts/utils/sass.js
@@ -1,30 +1,46 @@
 const kebabCase = require('lodash.kebabcase')
 
 const colorsByName = {
+  veryLightBlue: '#8b98ab',
   lightBlue: '#1acfff',
   mediumBlue: '#33aece',
-  darkGrey: '#4a4a4a',
-  lightGrey: '#fcfcfc',
+
+  lightGreen: '#bbe265',
+  mediumGreen: '#70c946',
+  darkGreen: '#63D180',
+
+  veryVeryLightGrey: '#fcfcfc',
+  veryLightGrey: '#f7f7f7',
+  lightGrey: '#c5c5c5',
   mediumGrey: '#dcdddc',
-  middleGrey: '#777777',
-  pastelGrey: '#c5c5c5',
+  darkGrey: '#777777',
+  veryDarkGrey: '#4a4a4a',
+
   red: '#DD2C00'
 }
 
  const sass = Object.assign(colorsByName, {
-  mini: '30em',
-  sm: '40em', // 648px
-  md: '52em',
-  lg: '64em', // 1280px;
-  xl: '75em',
+  mini: '425px',
+  sm: '768px',
+  md: '1024px',
+  lg: '1440px',
+  xl: '2560px',
+  backgroundColor: colorsByName.veryLightGrey,
+  border: `1px solid ${colorsByName.lightGrey}`,
   borderRadius: '0.25rem',
+  buttonBorderRadius: '2rem',
   boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
   boxShadowHover: '0 0 10px rgba(0,0,0,0.4)',
-  fontFamily: '"Raleway", "Helvetica Neue", Arial, sans-serif',
-  totalBackgroundImage: 'linear-gradient(to bottom, $light-grey, white)',
-  lightFontColor: colorsByName.pastelGrey,
-  middleFontColor: colorsByName.middleGrey,
-  darkFontColor: colorsByName.darkGrey
+  darkFontColor: colorsByName.veryDarkGrey,
+  fontFamily: '"Source Sans Pro", sans-serif',
+  headerHeight: '5rem',
+  identityColor: colorsByName.darkGreen,
+  lightFontColor: colorsByName.lightGrey,
+  lightIdentityColor: '$light-green',
+  middleFontColor: colorsByName.darkGrey,
+  totalBackgroundImage: 'linear-gradient(to bottom, $very-light-grey, white)',
+  opacityTransition: 'all 1s ease-out',
+  resizeTransition: 'all 0.4s ease-out'
 })
 
 Object.keys(sass)
